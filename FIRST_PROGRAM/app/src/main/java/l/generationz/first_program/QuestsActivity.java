1f46b441a3cb5a32b84f483fcb9537e2909a3768
julia bashko <julia0615.jb@gmali.com>
2017-12-25 10:04:58 +0200

    adedd quests to firebase

--- FIRST_PROGRAM/app/src/main/java/l/generationz/first_program/QuestsActivity.java
+++ FIRST_PROGRAM/app/src/main/java/l/generationz/first_program/QuestsActivity.java
@@ -1,13 +1,19 @@
 package l.generationz.first_program;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,6 +21,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -27,8 +35,10 @@ import java.io.InputStreamReader;
 public class QuestsActivity extends AppCompatActivity {
 
     private String currentQuest = null;
-   private  TextView questNameView;
-   private TextView questDescriptionView;
+    private TextView questNameView;
+    private TextView questDescriptionView;
+    private TextView questTaskView;
+    private ImageView questImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,48 +47,77 @@ public class QuestsActivity extends AppCompatActivity {
 
         questNameView = findViewById(R.id.quest_name);
         questDescriptionView = findViewById(R.id.quest_description);
+        questTaskView = findViewById(R.id.quest_task);
+        questImage = findViewById(R.id.quest_image );
 
         Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
+        if (bundle != null) {
             currentQuest = bundle.getString("questname");
 
         }
-        if (currentQuest!= null){
+        if (currentQuest != null) {
             initLoadFromDbBtn();
         }
 
 
-
-
-
-
     }
+
+
+
+
+
     private void initLoadFromDbBtn() {
 
 
-               FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Quests/");
-                Query phoneQuery = myRef.orderByKey();
-                phoneQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
-                            if( singleSnapshot.getKey().equalsIgnoreCase(currentQuest) ){
-                                QuestDetails quest = singleSnapshot.getValue(QuestDetails.class);
-                                questNameView.setText(quest.getName());
-                                questDescriptionView.setText(quest.getDescription());
-                            }
-
-
-                        }
-
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Quests/");
+        Query phoneQuery = myRef.orderByKey();
+        phoneQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    if (singleSnapshot.getKey().equalsIgnoreCase(currentQuest)) {
+                        QuestDetails quest = singleSnapshot.getValue(QuestDetails.class);
+                        questNameView.setText(quest.getName());
+                        questDescriptionView.setText(quest.getDescription());
+                        questTaskView.setText(quest.getTask() );
+                        loadImageByName(quest.getImage());
                     }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
 
-                    }
-                });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
+    }
+    private void loadImageByName(String name) {
+
+
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageRef = firebaseStorage.getReference();
+        StorageReference pathReference = storageRef.child("Quests/"+name);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        pathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                if (bytes.length > 0) {
+                    Bitmap myBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    questImage.setImageBitmap(myBitmap);
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
     }
 }
