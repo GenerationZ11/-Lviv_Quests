f90cba27d1ea1eac01a7cf10752a1510c243bf4d
Sviatoslav Yatsynyk <sviatik1208@gmail.com>
2018-05-04 23:25:33 +0300

    Added Project with REST service

---
+++ Project/app/src/main/java/com/company/lvivquest/project/MainActivity.java
@@ -0,0 +1,58 @@
+package com.company.lvivquest.project;
+
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    String server_url = "http://192.168.1.3:81/1.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         button = (Button) findViewById(R.id.btn);
+        textView = (TextView) findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        textView.setText(response);
+                        requestQueue.stop();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Бро нічого у тебе не вийшло");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                }
+                );
+                requestQueue.add(stringRequest);
+
+            }
+
+        }
+        );
+    }
+}
